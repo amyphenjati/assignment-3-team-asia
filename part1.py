9315df3d638a93a99a5a8b221abe8d86a5a50b7e
Jan Sirimongkolkasem <psirimongkolkasem1@babson.edu>
2021-03-23 17:36:21 +0700

    updated

---
+++ part1.py
@@ -0,0 +1,18 @@
+import urllib.request
+import json
+from pprint import pprint
+
+MAPQUEST_API_KEY = '9UevApDckPiKd1cnvkVJL2ZvZcQZNH6z'
+MBTA_API_KEY = 'c8640baae96c4213b1fd130df032069c'
+MAPQUEST_BASE_URL = "http://open.mapquestapi.com/geocoding/v1/address"
+MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
+
+### Write a function (maybe two) to extract the latitude and longitude from the JSON response.###
+
+
+
+### Write a function that takes an address or place name as input and returns a properly encoded URL to make a MapQuest geocode request.###
+
+
+
+### Write a function that takes a latitude and longitude and returns the name of the closest MBTA stop and whether it is wheelchair accessible. ###
