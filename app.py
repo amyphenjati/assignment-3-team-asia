bda77966a31486caa327103b8c2f28d60fd48a9c
amyphenji <77630330+amyphenjati@users.noreply.github.com>
2021-03-27 16:00:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 
+# Upon visiting the index page at http://127.0.0.1:5000/, the user will be greeted by a page that says hello, and includes an input form that requests a place name.
 # Upon clicking the 'Submit' button, the data from the form will be sent via a POST request to the Flask backend at the route POST /nearest
 # (Optional) Perform some simple validation on the user input. See wtforms.
 # The Flask backend will handle the request to POST /nearest_mbta. Then your app will render a mbta_station page for the user - presenting nearest MBTA stop and whether it is wheelchair accessible. In this step, you need to use the code from Part 1.
 # If something is wrong, the app will render a simple error page, which will include some indication that the search did not work, in addition to a button (or link) that will redirect the user back to the home page.
+
 
 from flask import Flask, render_template, request
 
